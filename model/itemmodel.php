96ea75155ed6ba60f5f3b533be8f6aa55e1bfc4a
Marlon Prado - WINDOWS - GITHUB DESKTOP <marlonprado04@gmail.com>
2023-11-25 18:08:32 -0300

    Revert "Revert "add: adicionei código de ItemModel""
    This reverts commit dd840bcdc7221d5f1ad84828381c5dbafba373ec.

---
+++ model/itemmodel.php
@@ -0,0 +1,54 @@
+<?php
+class ItemModel {
+    private $id;
+    private $imagem;
+    private $nome;
+    private $descricao;
+    private $preco;
+
+    // Construtor
+    public function __construct($id, $imagem, $nome, $descricao, $preco) {
+        $this->id = $id;
+        $this->imagem = $imagem;
+        $this->nome = $nome;
+        $this->descricao = $descricao;
+        $this->preco = $preco;
+    }
+
+    // Métodos Getters e Setters
+    public function getId() {
+        return $this->id;
+    }
+    
+    public function getImagem() {
+        return $this->imagem;
+    }
+
+    public function setImagem($imagem) {
+        $this->imagem = $imagem;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getDescricao() {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao) {
+        $this->descricao = $descricao;
+    }
+
+    public function getPreco() {
+        return $this->preco;
+    }
+
+    public function setPreco($preco) {
+        $this->preco = $preco;
+    }
+}
