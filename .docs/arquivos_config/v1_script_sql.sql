4dc2cc5499f9ae508324792d2e54433818017a73
Marlon Prado - WINDOWS - GITHUB DESKTOP <marlonprado04@gmail.com>
2023-11-27 21:28:17 -0300

    docs: add auto_increment em id no script sql

--- .docs/arquivos_config/v1_script_sql.sql
+++ .docs/arquivos_config/v1_script_sql.sql
@@ -4,20 +4,20 @@ USE poa_burguer;
 
 -- Criação da tabela 'mesa' se ela não existir
 CREATE TABLE IF NOT EXISTS mesa (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     numero_mesa INT
 );
 
 -- Criação da tabela 'comanda_mesa' se ela não existir
 CREATE TABLE IF NOT EXISTS comanda_mesa (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     id_comanda INT,
     id_mesa INT
 );
 
 -- Criação da tabela 'comanda' se ela não existir
 CREATE TABLE IF NOT EXISTS comanda (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     numero_comanda INT,
     id_comanda_mesa INT
 );
@@ -29,7 +29,7 @@ ALTER TABLE comanda_mesa ADD FOREIGN KEY (id_mesa) REFERENCES mesa(id);
 
 -- Criação da tabela 'item' se ela não existir
 CREATE TABLE IF NOT EXISTS item (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255),
     preco DOUBLE,
     descricao VARCHAR(255),
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS item (
 
 -- Criação da tabela 'item_comanda' se ela não existir
 CREATE TABLE IF NOT EXISTS item_comanda (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     id_item INT,
     id_comanda INT,
     quantidade INT,
