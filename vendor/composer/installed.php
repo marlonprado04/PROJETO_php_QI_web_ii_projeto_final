30dc8dc4acddd06261e3e863cdafde73a70fc299
Marlon Prado <marlonprado04@gmail.com>
2023-12-02 11:17:04 -0300

    atualizei o composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'name' => 'marlon/qi_web_ii_projeto_final',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'f60c59ec0825ba837f17b5473b1bfbe5f3d71ad7',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
+        'reference' => 'e916f5f27c2d119528f07f434c3340ccd976f2b3',
+        'name' => 'marlon/qi_web_ii_projeto_final',
         'dev' => true,
     ),
     'versions' => array(
         'marlon/qi_web_ii_projeto_final' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'f60c59ec0825ba837f17b5473b1bfbe5f3d71ad7',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
+            'reference' => 'e916f5f27c2d119528f07f434c3340ccd976f2b3',
             'dev_requirement' => false,
         ),
     ),
