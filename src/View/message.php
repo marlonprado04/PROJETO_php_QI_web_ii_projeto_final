09f15d47c2867da0233e561681b02c8943ac24c8
Marlon Prado - WINDOWS - GITHUB DESKTOP <marlonprado04@gmail.com>
2023-12-07 20:29:01 -0300

    removi mensagem com detalhes dos itens

--- src/View/message.php
+++ src/View/message.php
@@ -59,26 +59,6 @@
   // Se variável de sessão "msg_sucess"" não estiver vazia
   if (!empty($_SESSION["msg_success"])):
     ?>
-    <!-- Imprime um parágrafo com a mensagem armazenada na sessão -->
-    <div>
-      <p>
-        <?= $_SESSION["msg_success"] ?>
-      </p>
-
-      <p>Observacao:
-        <?= $_SESSION["item_observation"]  ?>
-      </p>
-      <p>Preço:
-        <?= $_SESSION["item_price"]  ?>
-      </p>
-      <p>Quantidade:
-        <?=  $_SESSION["item_quantity"] ?>
-      </p>
-      <p>ID:
-        <?= $_SESSION["item_id"] ?>
-      </p>
-
-    </div>
     <?php
     // Depois "destrói" a variável armazenada na sessão
     unset($_SESSION["msg_success"]);
